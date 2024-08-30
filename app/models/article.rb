ab123c1755da332bcf61bbdca2e0e8ea8a86baff
DESKTOP-2STJP7I\user <tomoyuki.enomoto@gmail.com>
2024-08-30 16:14:30 +0900

    add authentication files

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /A(?!\@)/ }
+  validates :title, format: { with: /A(?!@)/ }
 
   validates :content, presence: true
   validates :content, length: { minimum: 10 }
@@ -22,11 +22,12 @@ class Article < ApplicationRecord
     I18n.l(created_at, format: :default)
   end
 
-  private 
+  private
+
   def validate_title_and_content_length
-      char_count = self.title.length + self.content.length
-      unless char_count > 100
-        errors.add(:content, '100文字以上で!')
-      end
+    char_count = title.length + content.length
+    return if char_count > 100
+
+    errors.add(:content, '100文字以上で!')
   end
 end
